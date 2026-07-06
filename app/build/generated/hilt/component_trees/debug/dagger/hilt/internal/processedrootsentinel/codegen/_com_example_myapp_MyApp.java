7cc85a72caaa2573085fd24dc9be11419ffd4d30
legato-sonata <291633006+legato-sonata@users.noreply.github.com>
2026-07-06 08:39:11 +0000

    fist build

---
+++ app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_example_myapp_MyApp.java
@@ -0,0 +1,9 @@
+package dagger.hilt.internal.processedrootsentinel.codegen;
+
+import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
+
+@ProcessedRootSentinel(
+    roots = "com.example.myapp.MyApp"
+)
+public final class _com_example_myapp_MyApp {
+}
---
+++ app/build/kspCaches/debug/backups/java/byRounds/1/dagger/hilt/internal/aggregatedroot/codegen/_com_example_myapp_MyApp.java
@@ -0,0 +1,21 @@
+package dagger.hilt.internal.aggregatedroot.codegen;
+
+import dagger.hilt.android.HiltAndroidApp;
+import dagger.hilt.internal.aggregatedroot.AggregatedRoot;
+import javax.annotation.processing.Generated;
+
+/**
+ * This class should only be referenced by generated code! This class aggregates information across multiple compilations.
+ */
+@AggregatedRoot(
+    root = "com.example.myapp.MyApp",
+    rootPackage = "com.example.myapp",
+    originatingRoot = "com.example.myapp.MyApp",
+    originatingRootPackage = "com.example.myapp",
+    rootAnnotation = HiltAndroidApp.class,
+    rootSimpleNames = "MyApp",
+    originatingRootSimpleNames = "MyApp"
+)
+@Generated("dagger.hilt.processor.internal.root.AggregatedRootGenerator")
+public class _com_example_myapp_MyApp {
+}
